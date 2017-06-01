4b9dbe96c0da93c5b36f144a707e23397de6794e
Bohdan <b.borkivskyy@ucu.edu.ua>
2017-06-01 21:03:22 +0300

    v 0.6.4

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'SkyTravel',
-  packages = ['SkyTravel'],
-  version = '0.6.3',
+  packages = ['skytravel'],
+  version = '0.6.4',
   description = 'Module for searching flight tickets based on Skyscanner API',
   author = 'Bohdan Borkivskyy',
   author_email = 'b.borkivskyy@ucu.edu.ua',
   url = 'https://github.com/BohdanBorkivskyy/SkyTravel',
-  download_url = 'https://github.com/BohdanBorkivskyy/SkyTravel/archive/0.6.3.tar.gz',
+  download_url = 'https://github.com/BohdanBorkivskyy/SkyTravel/archive/0.6.4.tar.gz',
   keywords = ['travel', 'skyscanner', 'flight'],
   classifiers = [],
   install_requires=['pygame==1.9.3', 'skyscanner==1.1.4']
